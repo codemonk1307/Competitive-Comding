c2bc59de952fae5cd7172f206562e57e60dd695f
codemonk1307 <sachinmishra030@gmail.com>
2024-06-08 22:19:35 +0530

    Modified little bit 2sum prob of leetcode

--- Leetcode/1. Two Sum.py
+++ Leetcode/1. Two Sum.py
@@ -30,7 +30,7 @@ Follow-up: Can you come up with an algorithm that is less than O(n2) time comple
 
 
 
-
+from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # 
