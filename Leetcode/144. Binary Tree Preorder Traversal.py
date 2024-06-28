b3025ee315a0a319ad1487fa41687a11721300d3
codemonk1307 <sachinmishra030@gmail.com>
2024-06-28 23:57:41 +0530

    144 preorder binary tree traversal

--- Leetcode/144. Binary Tree Preorder Traversal.py
+++ Leetcode/144. Binary Tree Preorder Traversal.py
@@ -2,13 +2,13 @@
 
 
 '''
-145. Binary Tree Postorder Traversal -> Problem Statement
+144. Binary Tree Preorder Traversal -> Problem Statement
 
-Given the root of a binary tree, return the postorder traversal of its nodes' values.
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
 Example 1:
 Input: root = [1,null,2,3]
-Output: [3,2,1]
+Output: [1,2,3]
 
 Example 2:
 Input: root = []
@@ -19,7 +19,7 @@ Input: root = [1]
 Output: [1]
 
 Constraints:
-The number of the nodes in the tree is in the range [0, 100].
+The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 '''
 
@@ -32,19 +32,16 @@ The number of the nodes in the tree is in the range [0, 100].
 #         self.right = right
 
 class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        
-        # Postorder Traversal is a DFS Depth First Search Traversal 
-        # left  ->  right  ->  root 
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Recursive Approach
         res = []
         def dfs(root):
             #
             if root == None:
                 return
-            #
+            res.append(root.val)
             dfs(root.left)
             dfs(root.right)
-            res.append(root.val)
             #
             return res
         #
