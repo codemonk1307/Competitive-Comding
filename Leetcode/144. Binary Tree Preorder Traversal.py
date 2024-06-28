f7543bf5336da72a565b189a3f96a0c09dfa8db9
codemonk1307 <sachinmishra030@gmail.com>
2024-06-29 00:01:56 +0530

    144 preorder multiple approaches

--- Leetcode/144. Binary Tree Preorder Traversal.py
+++ Leetcode/144. Binary Tree Preorder Traversal.py
@@ -1,6 +1,5 @@
 
 
-
 '''
 144. Binary Tree Preorder Traversal -> Problem Statement
 
@@ -31,6 +30,8 @@ The number of nodes in the tree is in the range [0, 100].
 #         self.left = left
 #         self.right = right
 
+
+#--- APPROACH - 1 ------- Recursive Approach ---------APPROACH - 1 ------- Recursive Approach-------------- STARTS
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         # Recursive Approach
@@ -46,3 +47,26 @@ class Solution:
             return res
         #
         return dfs(root)
+#--- APPROACH - 1 ------- Recursive Approach ---------APPROACH - 1 ------- Recursive Approach-------------- ENDS
+
+
+
+#--- APPROACH - 2 ------- Iterative Approach ---------APPROACH - 2 ------- Iterative Approach-------------- STARTS
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Iterative Preorder -> DFS Traversal ||  Root  ->   left    ->   right
+        if not root:
+            return []
+        #
+        res, stack = [], [root]
+        while stack:
+            currNode = stack.pop()
+            if currNode:
+                res.append(currNode.val)
+                stack.append(currNode.right)
+                stack.append(currNode.left)
+        #        
+        return res
+
+#--- APPROACH - 2 ------- Iterative Approach ---------APPROACH - 2 ------- Iterative Approach-------------- ENDS
