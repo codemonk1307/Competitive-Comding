6e10436f954e1a279112f450ac27c0d217bbf2da
codemonk1307 <sachinmishra030@gmail.com>
2024-07-22 23:22:33 +0530

    added prob link for merge sorted array

--- Leetcode/88. Merge Sorted Array.py
+++ Leetcode/88. Merge Sorted Array.py
@@ -1,7 +1,7 @@
 
 
 '''
-88. Merge Sorted Array -> Problem Statement
+88. Merge Sorted Array -> Problem Statement (https://leetcode.com/problems/merge-sorted-array/)
 
 You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
 representing the number of elements in nums1 and nums2 respectively.
