03c804789988bd522bc581d22fffd377775f3aa5
codemonk1307 <sachinmishra030@gmail.com>
2025-02-03 00:04:16 +0530

    Binary Tree Level Order BFS Traversal Iterative Approach

--- GFG(Geeks for Geeks)/Level Order Traversal.py
+++ GFG(Geeks for Geeks)/Level Order Traversal.py
@@ -3,7 +3,6 @@
 """
 Level order traversal -> Problem Statement( geeksforgeeks.org/problems/level-order-traversal/1 )
 
-
 Given a root of a binary tree with n nodes, the task is to find its level order traversal. 
 Level order traversal of a tree is breadth-first traversal for the tree.
 
@@ -18,12 +17,10 @@ Output: [[10], [20, 30], [40, 50]]
 Input: root[] = [1, 3, 2, N, N, N, 4, 6, 5]
 Output: [[1], [3, 2], [4], [6, 5]]
 
-
 Constraints
 1 ≤ number of nodes ≤ 105
 0 ≤ node->data ≤ 109
 """
-
 
 
 
